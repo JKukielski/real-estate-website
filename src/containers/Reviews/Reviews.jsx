714c3a7b7869f55f8c896c9e6fdb010cd2c5deb9
Jasiu Kukielski <jankukielski1@gmail.com>
2022-06-27 22:12:59 +0200

    reviews responsiveness complete

--- src/containers/Reviews/Reviews.jsx
+++ src/containers/Reviews/Reviews.jsx
@@ -4,7 +4,9 @@ import { FaQuoteRight } from 'react-icons/fa'
 
 const Reviews = () => {
   return (
-    <div className="app__reviews">
+    <div className="app__reviews-section">
+      <h1 className="app__reviews-heading">Valued By Our Customers</h1>
+      <div className="app__reviews">
       <div className="app__review-container">
         <FaQuoteRight fontSize={72} color="var(--color-cream)"/>
         <p className="app__review-content">All properties that were shown to me by J&K Properties were wonderful. In the end the toughest part was the decision on which house to choose but everything was simply a pleasure. I would recommend J&K Properites to everyone.</p>
@@ -19,6 +21,7 @@ const Reviews = () => {
         <FaQuoteRight fontSize={72} color="var(--color-cream)"/>
         <p className="app__review-content">I received excellent service from J&K Properties! Whenever I needed some information or had some doubts, I was always able to reach out to someone from the agency for help.</p>
         <p className="app__review-name">Ellie H.</p>
+      </div>
       </div>
     </div>
   )

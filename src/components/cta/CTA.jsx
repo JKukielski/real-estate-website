7420434c5cb931930bd91289a33bc241c38f1d51
Jasiu Kukielski <jankukielski1@gmail.com>
2022-08-02 20:01:48 +0200

    CTA section done withour responsiveness

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import './cta.css'
+import React from "react";
+import "./cta.css";
 
 const CTA = () => {
   return (
-    <div>
-      CTA
+    <div className="app__cta">
+      <h1 className="app__cta-heading">Subscribe to our newsletter</h1>
+      <div className="app__cta-input_container">
+        <input
+          type="text"
+          placeholder="Enter your email address to subscribe..."
+        />
+        <button className="app__cta-input_btn">Subscribe</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
